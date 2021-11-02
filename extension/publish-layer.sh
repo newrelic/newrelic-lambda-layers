23124616b1716e166dbf2d67b503736d074c05f1
Michael Lavers <kolanos@gmail.com>
2021-11-02 15:32:24 -0700

    Add ARM64 Lambda Extension Publishing
    This is just the vanilla extension currently, the runtime specific layers are more involved.

--- extension/publish-layer.sh
+++ extension/publish-layer.sh
@@ -4,9 +4,11 @@ set -Eeuo pipefail
 
 BUCKET_PREFIX=nr-layers
 
-EXTENSION_DIST_DIR=extensions
-EXTENSION_DIST_URL=https://github.com/newrelic/newrelic-lambda-extension/releases/download/v2.0.4/newrelic-lambda-extension.zip
-EXTENSION_DIST_ZIP=extension.zip
+EXTENSION_DIST_URL_ARM64=https://github.com/newrelic/newrelic-lambda-extension/releases/download/v2.0.5/newrelic-lambda-extension.arm64.zip
+EXTENSION_DIST_URL_X86_64=https://github.com/newrelic/newrelic-lambda-extension/releases/download/v2.0.5/newrelic-lambda-extension.x86_64.zip
+
+EXTENSION_DIST_ZIP_ARM64=extension.arm64.zip
+EXTENSION_DIST_ZIP_X86_64=extension.x86_64.zip
 
 REGIONS=(
   ap-northeast-1
@@ -27,35 +29,46 @@ REGIONS=(
   us-west-2
 )
 
-function build-layer {
-    echo "Building New Relic Lambda Extension Layer"
-    rm -rf $EXTENSION_DIST_DIR $EXTENSION_DIST_ZIP
-    curl -L $EXTENSION_DIST_URL -o $EXTENSION_DIST_ZIP
+function build-layer-x86 {
+    echo "Building New Relic Lambda Extension Layer (x86_64)"
+    rm -f $EXTENSION_DIST_ZIP_X86_64
+
+    curl -L $EXTENSION_DIST_URL_X86_64 -o $EXTENSION_DIST_ZIP_X86_64
     echo "Build complete: ${EXTENSION_DIST_ZIP}"
 }
 
-function publish-layer {
-    if [ ! -f $EXTENSION_DIST_ZIP ]; then
-        echo "Package not found: ${EXTENSION_DIST_ZIP}"
+function build-layer-arm64 {
+    echo "Building New Relic Lambda Extension Layer (arm64)"
+    rm -f $EXTENSION_DIST_ZIP_ARM64
+
+    curl -L $EXTENSION_DIST_URL_ARM64 -o $EXTENSION_DIST_ZIP_ARM64
+
+    echo "Build complete: ${EXTENSION_DIST_ZIP}"
+}
+
+function publish-layer-x86 {
+    if [ ! -f $EXTENSION_DIST_ZIP_X86_64 ]; then
+        echo "Package not found: ${EXTENSION_DIST_ZIP_X86_64}"
         exit 1
     fi
 
-    layer_hash=$(md5sum $EXTENSION_DIST_ZIP | awk '{ print $1 }')
-    layer_s3key="nr-extension/${layer_hash}.zip"
+    layer_hash=$(md5sum $EXTENSION_DIST_ZIP_X86_64 | awk '{ print $1 }')
+    layer_s3key="nr-extension/${layer_hash}.x86_64.zip"
 
     for region in "${REGIONS[@]}"; do
         bucket_name="${BUCKET_PREFIX}-${region}"
 
-        echo "Uploading ${EXTENSION_DIST_ZIP} to s3://${bucket_name}/${layer_s3key}"
-        aws --region $region s3 cp $EXTENSION_DIST_ZIP "s3://${bucket_name}/${layer_s3key}"
+        echo "Uploading ${EXTENSION_DIST_ZIP_X86_64} to s3://${bucket_name}/${layer_s3key}"
+        aws --region $region s3 cp $EXTENSION_DIST_ZIP_X86_64 "s3://${bucket_name}/${layer_s3key}"
 
         echo "Publishing extension layer to ${region}"
         layer_version=$(aws lambda publish-layer-version \
             --layer-name NewRelicLambdaExtension \
             --content "S3Bucket=${bucket_name},S3Key=${layer_s3key}" \
-            --description "New Relic Lambda Extension Layer" \
+            --description "New Relic Lambda Extension Layer (x86_64)" \
             --license-info "Apache-2.0" \
             --compatible-runtimes "dotnetcore3.1" "provided" "provided.al2" \
+            --compatible-architectures "x86_64" \
             --region $region \
             --output text \
             --query Version)
@@ -73,5 +86,48 @@ function publish-layer {
     done
 }
 
-build-layer
-publish-layer
+function publish-layer-arm64 {
+    if [ ! -f $EXTENSION_DIST_ZIP_ARM64 ]; then
+        echo "Package not found: ${EXTENSION_DIST_ZIP_ARM64}"
+        exit 1
+    fi
+
+    layer_hash=$(md5sum $EXTENSION_DIST_ZIP_ARM64 | awk '{ print $1 }')
+    layer_s3key="nr-extension/${layer_hash}.arm64.zip"
+
+    for region in "${REGIONS[@]}"; do
+        bucket_name="${BUCKET_PREFIX}-${region}"
+
+        echo "Uploading ${EXTENSION_DIST_ZIP_ARM64} to s3://${bucket_name}/${layer_s3key}"
+        aws --region $region s3 cp $EXTENSION_DIST_ZIP_ARM64 "s3://${bucket_name}/${layer_s3key}"
+
+        echo "Publishing extension layer to ${region}"
+        layer_version=$(aws lambda publish-layer-version \
+            --layer-name NewRelicLambdaExtensionARM64 \
+            --content "S3Bucket=${bucket_name},S3Key=${layer_s3key}" \
+            --description "New Relic Lambda Extension Layer (arm64)" \
+            --license-info "Apache-2.0" \
+            --compatible-runtimes "dotnetcore3.1" "provided" "provided.al2" \
+            --compatible-architectures "arm64" \
+            --region $region \
+            --output text \
+            --query Version)
+        echo "Published layer version ${layer_version} to ${region}"
+
+        echo "Setting public permissions for layer version ${layer_version} in ${region}"
+        aws lambda add-layer-version-permission \
+          --layer-name NewRelicLambdaExtensionARM64 \
+          --version-number $layer_version \
+          --statement-id public \
+          --action lambda:GetLayerVersion \
+          --principal "*" \
+          --region $region
+        echo "Public permissions set for layer version ${layer_version} in region ${region}"
+    done
+}
+
+build-layer-x86
+publish-layer-x86
+
+build-layer-arm64
+publish-layer-arm64
