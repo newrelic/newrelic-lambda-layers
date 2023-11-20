bbe2fdd3b137fe5df61a70c97367adf9b7e9f63f
mrickard <maurice@mauricerickard.com>
2023-11-20 15:59:30 -0500

    chore: Refactored wrapper unit tests, to set env vars before the wrapper is loaded
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- nodejs/test/unit/cjsHandler.tap.js
+++ nodejs/test/unit/cjsHandler.tap.js
@@ -13,10 +13,12 @@ tap.test('Layer Handler - CJS Function', (t) => {
   t.beforeEach(() => {
     originalEnv = { ...process.env }
     process.env.NEW_RELIC_USE_ESM = 'false'
-    
-    helper = utils.TestAgent.makeInstrumented() 
+    process.env.NEW_RELIC_LAMBDA_HANDLER = 'test/unit/fixtures/cjs/handler.handler'
+    process.env.AWS_LAMBDA_FUNCTION_NAME = 'testFn'
 
-    const newrelic = helper.getAgentApi() 
+    helper = utils.TestAgent.makeInstrumented()
+
+    const newrelic = helper.getAgentApi()
 
     ;({ handler } = proxyquire('../../index', {
       'newrelic': newrelic
@@ -27,11 +29,8 @@ tap.test('Layer Handler - CJS Function', (t) => {
     process.env = { ...originalEnv }
     helper.unload()
   })
-  
-  t.test('should wrap handler in transaction', async(t) => {
-    process.env.NEW_RELIC_LAMBDA_HANDLER = 'test/unit/fixtures/cjs/handler.handler'
-    process.env.AWS_LAMBDA_FUNCTION_NAME = 'testFn'
 
+  t.test('should wrap handler in transaction', async(t) => {
     helper.agent.on('transactionFinished', (transaction) => {
       t.equal(transaction.name, 'OtherTransaction/Function/testFn', 'transaction should be properly named')
     })
--- nodejs/test/unit/esmHandler.tap.js
+++ nodejs/test/unit/esmHandler.tap.js
@@ -13,10 +13,12 @@ tap.test('Layer Handler - ESM Function', (t) => {
   t.beforeEach(() => {
     originalEnv = { ...process.env }
     process.env.NEW_RELIC_USE_ESM = 'true'
+    process.env.NEW_RELIC_LAMBDA_HANDLER = 'test/unit/fixtures/esm/handler.handler'
+    process.env.AWS_LAMBDA_FUNCTION_NAME = 'testFn'
 
-    helper = utils.TestAgent.makeInstrumented() 
+    helper = utils.TestAgent.makeInstrumented()
 
-    const newrelic = helper.getAgentApi() 
+    const newrelic = helper.getAgentApi()
 
     ;({ handler } = proxyquire('../../index', {
       'newrelic': newrelic
@@ -27,11 +29,8 @@ tap.test('Layer Handler - ESM Function', (t) => {
     process.env = { ...originalEnv }
     helper.unload()
   })
-  
-  t.test('should wrap handler in transaction', async(t) => {
-    process.env.NEW_RELIC_LAMBDA_HANDLER = 'test/unit/fixtures/esm/handler.handler'
-    process.env.AWS_LAMBDA_FUNCTION_NAME = 'testFn'
 
+  t.test('should wrap handler in transaction', async(t) => {
     helper.agent.on('transactionFinished', (transaction) => {
       t.equal(transaction.name, 'OtherTransaction/Function/testFn', 'transaction should be properly named')
     })
