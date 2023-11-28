5592771731edfc521189997c8be2d9d034c9ef16
mrickard <maurice@mauricerickard.com>
2023-11-28 16:25:13 -0500

    feat: Added publishing of Node 20 layers
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- nodejs/publish-layers.sh
+++ nodejs/publish-layers.sh
@@ -9,12 +9,14 @@ source ../libBuild.sh
 
 NJS16X_DIST_ARM64=$DIST_DIR/nodejs16x.arm64.zip
 NJS18X_DIST_ARM64=$DIST_DIR/nodejs18x.arm64.zip
+NJS20X_DIST_ARM64=$DIST_DIR/nodejs20x.arm64.zip
 
 NJS16X_DIST_X86_64=$DIST_DIR/nodejs16x.x86_64.zip
 NJS18X_DIST_X86_64=$DIST_DIR/nodejs18x.x86_64.zip
+NJS20X_DIST_X86_64=$DIST_DIR/nodejs20x.x86_64.zip
 
 function usage {
-  	echo "./publish-layers.sh [nodejs16x|nodejs18x]"
+  	echo "./publish-layers.sh [nodejs16x|nodejs18x|nodejs20x]"
 }
 
 function build-nodejs16x-arm64 {
@@ -113,6 +115,54 @@ function publish-nodejs18x-x86 {
     done
 }
 
+function build-nodejs20x-arm64 {
+	echo "Building new relic layer for nodejs20.x (arm64)"
+	rm -rf $BUILD_DIR $NJS20X_DIST_ARM64
+	mkdir -p $DIST_DIR
+	npm install --prefix $BUILD_DIR newrelic@latest
+	mkdir -p $BUILD_DIR/node_modules/newrelic-lambda-wrapper
+	cp index.js $BUILD_DIR/node_modules/newrelic-lambda-wrapper
+	download_extension arm64
+	zip -rq $NJS20X_DIST_ARM64 $BUILD_DIR $EXTENSION_DIST_DIR $EXTENSION_DIST_PREVIEW_FILE
+	rm -rf $BUILD_DIR $EXTENSION_DIST_DIR $EXTENSION_DIST_PREVIEW_FILE
+	echo "Build complete: ${NJS20X_DIST_ARM64}"
+}
+
+function build-nodejs20x-x86 {
+	echo "Building new relic layer for nodejs20.x (x86_64)"
+	rm -rf $BUILD_DIR $NJS20X_DIST_X86_64
+	mkdir -p $DIST_DIR
+	npm install --prefix $BUILD_DIR newrelic@latest
+	mkdir -p $BUILD_DIR/node_modules/newrelic-lambda-wrapper
+	cp index.js $BUILD_DIR/node_modules/newrelic-lambda-wrapper
+	download_extension x86_64
+	zip -rq $NJS20X_DIST_X86_64 $BUILD_DIR $EXTENSION_DIST_DIR $EXTENSION_DIST_PREVIEW_FILE
+	rm -rf $BUILD_DIR $EXTENSION_DIST_DIR $EXTENSION_DIST_PREVIEW_FILE
+	echo "Build complete: ${NJS20X_DIST_X86_64}"
+}
+
+function publish-nodejs20x-arm64 {
+    if [ ! -f $NJS20X_DIST_ARM64 ]; then
+      echo "Package not found: ${NJS20X_DIST_ARM64}"
+      exit 1
+    fi
+
+    for region in "${REGIONS_ARM[@]}"; do
+      publish_layer $NJS20X_DIST_ARM64 $region nodejs20.x arm64
+    done
+}
+
+function publish-nodejs20x-x86 {
+    if [ ! -f $NJS20X_DIST_X86_64 ]; then
+      echo "Package not found: ${NJS20X_DIST_X86_64}"
+      exit 1
+    fi
+
+    for region in "${REGIONS_X86[@]}"; do
+      publish_layer $NJS20X_DIST_X86_64 $region nodejs20.x x86_64
+    done
+}
+
 case "$1" in
 "build-nodejs16x")
 	build-nodejs16x-arm64
@@ -130,6 +180,14 @@ case "$1" in
 	publish-nodejs18x-arm64
 	publish-nodejs18x-x86
 	;;
+"build-nodejs20x")
+	build-nodejs20x-arm64
+	build-nodejs20x-x86
+	;;
+"publish-nodejs20x")
+	publish-nodejs20x-arm64
+	publish-nodejs20x-x86
+	;;
 "nodejs16x")
 	$0 build-nodejs16x
 	$0 publish-nodejs16x
@@ -137,6 +195,10 @@ case "$1" in
 "nodejs18x")
 	$0 build-nodejs18x
 	$0 publish-nodejs18x
+	;;
+"nodejs20x")
+	$0 build-nodejs20x
+	$0 publish-nodejs20x
 	;;
 *)
 	usage
