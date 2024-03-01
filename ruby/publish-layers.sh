2a0e74abf14546868c51fb28d71e69ebff2010e2
fallwith <fallwith@gmail.com>
2024-03-01 01:59:19 -0800

    Ruby publish script updates
    * typo fix 'subdir' -> 'sub_dir' variable
    * include special packaging logic to support sourcing the agent from
      GitHub instead of RubyGems.org

--- ruby/publish-layers.sh
+++ ruby/publish-layers.sh
@@ -35,13 +35,34 @@ function build_and_publish_ruby_for_arch {
   bundle install
 
   local base_dir="vendor/bundle/ruby/${ruby_version}.0"
+
+  # allow Ruby versions other than the target version to bundle
   mv vendor/bundle/ruby/* $base_dir
 
-  for subdir in 'bin' 'build_info' 'cache' 'doc' 'extensions' 'plugins'; do
+  for sub_dir in 'bin' 'build_info' 'cache' 'doc' 'extensions' 'plugins'; do
     rm -rf $base_dir/$sub_dir
   done
 
+  # if Gemfile points Bundler to GitHub for the agent, discard extraneous repo
+  # content and repackage the vendored gem content as if it were sourced
+  # from RubyGems.org
+  if [[ -e "$base_dir/bundler" ]]; then
+    local phony_version=1.1.38
+    mkdir $base_dir/gems
+    local nr_dir=$base_dir/gems/newrelic_rpm-$phony_version
+    mv $base_dir/bundler/gems/newrelic-ruby-agent* $nr_dir
+    rm -rf $base_dir/bundler
+
+    mkdir $base_dir/specifications
+    echo -e "Gem::Specification.new {|s| s.name = 'newrelic_rpm'; s.version = '$phony_version'}" > $base_dir/specifications/newrelic_rpm-$phony_version.gemspec
+
+    for sub_dir in '.git' '.github' '.gitignore' '.rubocop.yml' '.rubocop_todo.yml' '.simplecov' '.snyk' '.yardopts' 'Brewfile' 'config' 'CONTRIBUTING.md' 'docker-compose.yml' 'DOCKER.md' 'Dockerfile' 'Gemfile' 'Guardfile' 'infinite_tracing' 'init.rb' 'install.rb' 'lefthook.yml' 'newrelic.yml' 'README.md' 'test' 'THIRD_PARTY_NOTICES.md' 'Thorfile' 'recipes' '.build_ignore'; do
+      rm -rf "$nr_dir/$sub_dir"
+    done
+  fi
+
   download_extension $arch
+
   zip -rq $dist_file $WRAPPER_FILE $BUILD_DIR $EXTENSION_DIST_DIR $EXTENSION_DIST_PREVIEW_FILE
   rm -rf $BUILD_DIR $BUILD_DIR $EXTENSION_DIST_DIR $EXTENSION_DIST_PREVIEW_FILE
   echo "Build complete: ${dist_file}"
