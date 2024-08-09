57122f88fc82cbcd7d3480638c16a3cc572ee8ae
Saket Chaudhary <118331906+chaudharysaket@users.noreply.github.com>
2024-08-09 09:23:13 +0530

    release layers for extension v2.3.13 (#267)

--- libBuild.sh
+++ libBuild.sh
@@ -60,7 +60,7 @@ EXTENSION_DIST_DIR=extensions
 EXTENSION_DIST_ZIP=extension.zip
 EXTENSION_DIST_PREVIEW_FILE=preview-extensions-ggqizro707
 
-EXTENSION_VERSION=2.3.12
+EXTENSION_VERSION=2.3.13
 
 function list_all_regions {
     aws ec2 describe-regions \
