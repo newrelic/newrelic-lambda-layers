95dbbdfa698e72cc465498a83e86a48cae75fc84
Maurice Rickard <maurice@mauricerickard.com>
2024-11-27 23:37:50 -0500

    feat: Added publishing for Node 22 layers (#297)
    * chore: Updated Node agent version used in tests
    
    Signed-off-by: mrickard <maurice@mauricerickard.com>
    
    * feat: Added Node 22 publishing
    
    Signed-off-by: mrickard <maurice@mauricerickard.com>
    
    ---------
    
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- nodejs/publish-layers.sh
+++ nodejs/publish-layers.sh
@@ -113,6 +113,54 @@ function publish-nodejs20x-x86 {
     done
 }
 
+function build-nodejs22x-arm64 {
+	echo "Building new relic layer for nodejs22.x (arm64)"
+	rm -rf $BUILD_DIR $NJS22X_DIST_ARM64
+	mkdir -p $DIST_DIR
+	npm install --prefix $BUILD_DIR newrelic@latest
+	mkdir -p $BUILD_DIR/node_modules/newrelic-lambda-wrapper
+	cp index.js $BUILD_DIR/node_modules/newrelic-lambda-wrapper
+	download_extension arm64
+	zip -rq $NJS22X_DIST_ARM64 $BUILD_DIR $EXTENSION_DIST_DIR $EXTENSION_DIST_PREVIEW_FILE
+	rm -rf $BUILD_DIR $EXTENSION_DIST_DIR $EXTENSION_DIST_PREVIEW_FILE
+	echo "Build complete: ${NJS22X_DIST_ARM64}"
+}
+
+function build-nodejs22x-x86 {
+	echo "Building new relic layer for nodejs22.x (x86_64)"
+	rm -rf $BUILD_DIR $NJS22X_DIST_X86_64
+	mkdir -p $DIST_DIR
+	npm install --prefix $BUILD_DIR newrelic@latest
+	mkdir -p $BUILD_DIR/node_modules/newrelic-lambda-wrapper
+	cp index.js $BUILD_DIR/node_modules/newrelic-lambda-wrapper
+	download_extension x86_64
+	zip -rq $NJS22X_DIST_X86_64 $BUILD_DIR $EXTENSION_DIST_DIR $EXTENSION_DIST_PREVIEW_FILE
+	rm -rf $BUILD_DIR $EXTENSION_DIST_DIR $EXTENSION_DIST_PREVIEW_FILE
+	echo "Build complete: ${NJS22X_DIST_X86_64}"
+}
+
+function publish-nodejs22x-arm64 {
+    if [ ! -f $NJS22X_DIST_ARM64 ]; then
+      echo "Package not found: ${NJS22X_DIST_ARM64}"
+      exit 1
+    fi
+
+    for region in "${REGIONS_ARM[@]}"; do
+      publish_layer $NJS22X_DIST_ARM64 $region nodejs22.x arm64
+    done
+}
+
+function publish-nodejs22x-x86 {
+    if [ ! -f $NJS22X_DIST_X86_64 ]; then
+      echo "Package not found: ${NJS22X_DIST_X86_64}"
+      exit 1
+    fi
+
+    for region in "${REGIONS_X86[@]}"; do
+      publish_layer $NJS22X_DIST_X86_64 $region nodejs22.x x86_64
+    done
+}
+
 case "$1" in
 "build-nodejs18x")
 	build-nodejs18x-arm64
@@ -130,6 +178,14 @@ case "$1" in
 	publish-nodejs20x-arm64
 	publish-nodejs20x-x86
 	;;
+"build-nodejs22x")
+	build-nodejs22x-arm64
+	build-nodejs22x-x86
+	;;
+"publish-nodejs22x")
+	publish-nodejs22x-arm64
+	publish-nodejs22x-x86
+	;;
 "build-publish-nodejs18x-ecr-image")
 	build-nodejs18x-arm64
 	publish_docker_ecr $NJS18X_DIST_ARM64 nodejs18.x arm64
@@ -142,6 +198,12 @@ case "$1" in
 	build-nodejs20x-x86
 	publish_docker_ecr $NJS20X_DIST_X86_64 nodejs20.x x86_64
 	;;
+"build-publish-nodejs22x-ecr-image")
+	build-nodejs22x-arm64
+	publish_docker_ecr $NJS22X_DIST_ARM64 nodejs22.x arm64
+	build-nodejs22x-x86
+	publish_docker_ecr $NJS22X_DIST_X86_64 nodejs22.x x86_64
+	;;
 "nodejs18x")
 	$0 build-nodejs18x
 	$0 publish-nodejs18x
@@ -149,6 +211,10 @@ case "$1" in
 "nodejs20x")
 	$0 build-nodejs20x
 	$0 publish-nodejs20x
+	;;
+"nodejs22x")
+	$0 build-nodejs22x
+	$0 publish-nodejs22x
 	;;
 *)
 	usage
