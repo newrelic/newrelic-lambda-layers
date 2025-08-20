2c01bbbd18854d74a76c092dba84805c4ac366e9
Chodavarapu Naga Viswanadha Avinash <aavinash@newrelic.com>
2025-08-20 12:09:54 +0530

    Update libBuild.sh

--- libBuild.sh
+++ libBuild.sh
@@ -3,32 +3,32 @@
 set -Eeuo pipefail
 
 REGIONS=(
-  sa-east-1
-  me-central-1
-  me-south-1
-  eu-central-2
-  eu-north-1
-  eu-south-2
-  eu-west-3
-  eu-south-1
-  eu-west-2
-  eu-west-1
-  eu-central-1
-  ca-central-1
-  ap-northeast-1
-  ap-southeast-2
-  ap-southeast-1
-  ap-northeast-2
-  ap-northeast-3
-  ap-south-1
-  ap-south-2
-  ap-southeast-4
-  ap-southeast-3
-  af-south-1
+ #  sa-east-1
+ #  me-central-1
+ #  me-south-1
+ #  eu-central-2
+ #  eu-north-1
+ #  eu-south-2
+ #  eu-west-3
+ #  eu-south-1
+ #  eu-west-2
+ #  eu-west-1
+ #  eu-central-1
+ #  ca-central-1
+ #  ap-northeast-1
+ #  ap-southeast-2
+ #  ap-southeast-1
+ #  ap-northeast-2
+ #  ap-northeast-3
+ #  ap-south-1
+ #  ap-south-2
+ #  ap-southeast-4
+ #  ap-southeast-3
+ #  af-south-1
   us-east-1
-	us-east-2
+	# us-east-2
 	us-west-1
-	us-west-2
+	# us-west-2
 )
 
 EXTENSION_DIST_DIR=extensions
