9081feb7b5ed3c7be08b19b5cf82b2584b5f6900
Saket Chaudhary <saketchaudharynit@gmail.com>
2025-08-29 15:29:11 +0530

    release extension_v2.3.24

--- libBuild.sh
+++ libBuild.sh
@@ -35,7 +35,7 @@ EXTENSION_DIST_DIR=extensions
 EXTENSION_DIST_ZIP=extension.zip
 EXTENSION_DIST_PREVIEW_FILE=preview-extensions-ggqizro707
 
-EXTENSION_VERSION=2.3.23
+EXTENSION_VERSION=2.3.24
 
 function list_all_regions {
     aws ec2 describe-regions \
