677f9c6d8be9f170d4cdebc25d9c9f9e04fb1774
Chodavarapu Naga Viswanadha Avinash <aavinash@newrelic.com>
2026-03-14 01:06:52 +0530

    feat: add support for universal layers in Node.js and Python workflows

--- libBuild.sh
+++ libBuild.sh
@@ -26,9 +26,9 @@ REGIONS=(
   ap-southeast-3
   af-south-1
   us-east-1
-	us-east-2
-	us-west-1
-	us-west-2
+  us-east-2
+  us-west-1
+  us-west-2
 )
 
 EXTENSION_DIST_DIR=extensions
@@ -94,6 +94,9 @@ function layer_name_str() {
     "python3.14")
       rt_part="Python314"
       ;;
+    "python")
+      rt_part="Python"
+      ;;
     "nodejs20.x")
       rt_part="NodeJS20X"
       ;;
@@ -102,6 +105,9 @@ function layer_name_str() {
       ;;
     "nodejs24.x")
       rt_part="NodeJS24X"
+      ;;
+    "nodejs")
+      rt_part="NodeJS"
       ;;
     "ruby3.2")
       rt_part="Ruby32"
@@ -157,6 +163,9 @@ function s3_prefix() {
     "python3.14")
       name="nr-python3.14"
       ;;
+    "python")
+      name="nr-python"
+      ;;
     "nodejs20.x")
       name="nr-nodejs20.x"
       ;;
@@ -165,6 +174,9 @@ function s3_prefix() {
       ;;
     "nodejs24.x")
       name="nr-nodejs24.x"
+      ;;
+    "nodejs")
+      name="nr-nodejs"
       ;;
     "ruby3.3")
       name="nr-ruby3.3"
@@ -191,7 +203,7 @@ function agent_name_str() {
         "dotnet")
             agent_name="Dotnet"
             ;;
-        "nodejs20.x"|"nodejs22.x"|"nodejs24.x")
+        "nodejs"|"nodejs20.x"|"nodejs22.x"|"nodejs24.x")
             agent_name="Node"
             ;;
         "ruby3.2"|"ruby3.3"|"ruby3.4")
@@ -200,7 +212,7 @@ function agent_name_str() {
         "java8.al2"|"java11"|"java17"|"java21")
             agent_name="Java"
             ;;
-        "python3.9"|"python3.10"|"python3.11"|"python3.12"|"python3.13"|"python3.14")
+        "python"|"python3.9"|"python3.10"|"python3.11"|"python3.12"|"python3.13"|"python3.14")
             agent_name="Python"
             ;;
         *)
@@ -278,6 +290,14 @@ function publish_layer {
     then compat_list=("dotnet6" "dotnet8" "dotnet10")
     fi
 
+    if [[ $runtime_name == "python" ]]
+    then compat_list=("python3.9" "python3.10" "python3.11" "python3.12" "python3.13" "python3.14")
+    fi
+
+    if [[ $runtime_name == "nodejs" ]]
+    then compat_list=("nodejs20.x" "nodejs22.x" "nodejs24.x")
+    fi
+
     echo "Uploading ${layer_archive} to s3://${bucket_name}/${s3_key}"
     aws --region "$region" s3 cp $layer_archive "s3://${bucket_name}/${s3_key}"
 
@@ -340,11 +360,17 @@ function publish_docker_ecr {
     version_flag=$EXTENSION_VERSION
     language_flag="lambdaextension"
     fi
-    
+
     if [[ ${runtime_name} =~ 'dotnet' ]]; then
     version_flag=""
     arch_flag=${arch}
     fi
+
+    if [[ $runtime_name == "python" || $runtime_name == "nodejs" ]]; then
+    version_flag=""
+    arch_flag=${arch}
+    fi
+
     slim_flag=""
     if [ "$slim" == "slim" ]; then
         slim_flag="-slim"
@@ -361,7 +387,7 @@ function publish_docker_ecr {
 
     # public ecr repository name 
     # maintainer can use this("q6k3q1g1") repo name for testing 
-    repository="x6n7b2o2"
+    repository="x6n7b2o2""
 
     # copy dockerfile
     cp ../Dockerfile.ecrImage .
