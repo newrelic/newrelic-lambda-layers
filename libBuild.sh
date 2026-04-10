0f51c6bda2c91ba7f63c189b3439da1acc367a0c
Sashwatdas123 <Sashwatdas@newrelic.com>
2026-04-10 20:19:37 +0530

    feat: updated extension version to v2.5.0

--- libBuild.sh
+++ libBuild.sh
@@ -37,7 +37,7 @@ EXTENSION_DIST_DIR=extensions
 EXTENSION_DIST_ZIP=extension.zip
 EXTENSION_DIST_PREVIEW_FILE=preview-extensions-ggqizro707
 
-EXTENSION_VERSION=2.4.6
+EXTENSION_VERSION=2.5.0
 
 function list_all_regions {
     aws ec2 describe-regions \
