c43c35a4256e9bc97829d55a1bb81aba889b3998
Sashwatdas123 <Sashwatdas@newrelic.com>
2026-06-01 17:29:15 +0530

    Update libBuild.sh (#484)

--- libBuild.sh
+++ libBuild.sh
@@ -52,7 +52,7 @@ EXTENSION_DIST_DIR=extensions
 EXTENSION_DIST_ZIP=extension.zip
 EXTENSION_DIST_PREVIEW_FILE=preview-extensions-ggqizro707
 
-EXTENSION_VERSION=2.5.3
+EXTENSION_VERSION=2.6.0
 
 function list_all_regions {
     aws ec2 describe-regions \
